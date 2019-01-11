c3f7fe036b572aaf08b3f28e7745a35af7183326
Ghost_chu <2908803755@qq.com>
2019-01-11 17:46:44 +0800

    Push to github

---
+++ src/main/java/com/mcsunnyside/qstowny/QSRRTownyAddon.java
@@ -0,0 +1,75 @@
+package com.mcsunnyside.qstowny;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.maxgamer.quickshop.QuickShop;
+import org.maxgamer.quickshop.Events.ShopCreateEvent;
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Coord;
+import com.palmergames.bukkit.towny.object.TownBlock;
+import com.palmergames.bukkit.towny.object.TownBlockType;
+import com.palmergames.bukkit.towny.object.TownyUniverse;
+import com.palmergames.bukkit.towny.object.TownyWorld;
+import com.palmergames.bukkit.towny.object.WorldCoord;
+
+public class QSRRTownyAddon extends JavaPlugin implements Listener {
+	boolean fail2load = false;
+	QuickShop qs = null;
+	Towny towny = null;
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		qs = (QuickShop) Bukkit.getPluginManager().getPlugin("QuickShop");
+		towny = (Towny) Bukkit.getPluginManager().getPlugin("Towny");
+		//Verify plugin is reremake or reremake's forks
+		try {
+			QuickShop.getVersion();
+		}catch (Exception e) {
+			getLogger().severe("QSRR Towny Addon only can running under QuickShop-Reremake by Ghost_chu.");
+			fail2load=true;
+			Bukkit.getPluginManager().disablePlugin(this);
+		}finally {
+			if(fail2load)
+				return;
+		}
+		if(qs.special_region_only) {
+			getLogger().warning("You cannot use Towny Addon when you turn on Special_Region_Only features.");
+			fail2load=true;
+			Bukkit.getPluginManager().disablePlugin(this);
+		}
+		if(fail2load)
+			return;
+		saveDefaultConfig();
+		getLogger().info("Successfully loaded QSRR's towny addon.");
+		
+	}
+	@Override
+	public void onDisable() {
+		if(fail2load)
+			return;
+	}
+	@EventHandler
+	public void shopCreateEvent (ShopCreateEvent e) {
+		TownyWorld tWorld = TownyUniverse.getDataSource().getTownWorld(e.getShop().getLocation().getWorld().getName());
+		if(tWorld==null||!tWorld.isUsingTowny()){
+			return;//Not Towny World, ignore it
+		}
+		TownBlock townBlock = null;
+		try {
+			 townBlock = new WorldCoord(e.getShop().getLocation().getWorld().getName(), Coord.parseCoord(e.getPlayer())).getTownBlock();
+		} catch (NotRegisteredException e1) {
+			//Ignore
+		}
+		if(townBlock==null)
+			return; //WTF
+		TownBlockType tBlockType = townBlock.getType();
+		if(tBlockType!=TownBlockType.COMMERCIAL) {
+			e.setCancelled(true);
+			e.getPlayer().sendMessage(getConfig().getString("messages.not-shop-plot"));
+		}
+	}
+}
